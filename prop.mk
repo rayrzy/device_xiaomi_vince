cf974112926cd49b86e925121ae2da85457b78be
0ranko0P <ranko0p@outlook.com>
2020-07-06 01:54:07 +0800

    vince: Set zygote preforking in system_prop
    Move it to PRODUCT_SYSTEM_DEFAULT_PROPERTIES to fix violated by
    avc:  denied  { set } for property=persist.device_config.runtime_native.usap_pool_enabled pid=1 uid=0 gid=0
    scontext=u:r:vendor_init:s0
    tcontext=u:object_r:device_config_runtime_native_prop:s0 tclass=property_service
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- prop.mk
+++ prop.mk
@@ -246,5 +246,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 vendor.qc2.use.bqpool=1
 
 # USAP
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 persist.device_config.runtime_native.usap_pool_enabled=true
