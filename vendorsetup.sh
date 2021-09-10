5c9dcb217da35bd9c78ece4773e352fd0ebc8468
Erfan Abdi <erfangplus@gmail.com>
2021-09-10 20:54:14 +0200

    vince: prepare to move to in-tree HALs
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: I2e7fa967e2baa700cc91c862fbfd783fdae46bf0

--- vendorsetup.sh
+++
@@ -1,6 +0,0 @@
-rm -rf hardware/qcom-caf/msm8996/media
-rm -rf hardware/qcom-caf/msm8996/display
-rm -rf hardware/qcom-caf/msm8996/audio
-git clone https://github.com/rayrzy/hardware_qcom-caf_msm8996-r_audio hardware/qcom-caf/msm8996/audio
-git clone https://github.com/rayrzy/hardware_qcom-caf_msm8996-r_display hardware/qcom-caf/msm8996/display
-git clone https://github.com/rayrzy/hardware_qcom-caf_msm8996-r_media hardware/qcom-caf/msm8996/media
