b0bb2f44c86198d691ba469ef297dacb7c6907ab
GhostMaster69-dev <rathore6375@gmail.com>
2021-10-30 06:26:32 +0530

    vince: update powerhint from LA.UM.8.6.2.r1-08400-89xx.0
    * Also place a copy of pwerhint into system/etc
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- device.mk
+++ device.mk
@@ -357,6 +357,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
