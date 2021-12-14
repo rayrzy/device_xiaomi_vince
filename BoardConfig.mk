af0edd45440c2603e44af120577719c98c1cae16
rayrzy <vincentrayananda@gmail.com>
2021-12-14 14:24:50 +0700

    vince: use rzy clang for compilation
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,9 +61,9 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vince
 TARGET_KERNEL_VERSION := 4.9
-TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := rzyTC
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-rzy
+TARGET_KERNEL_ADDITIONAL_FLAGS := AR=llvm-ar AS=llvm-as NM=llvm-nm LD=ld.lld OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump OBJSIZE=llvm-size READELF=llvm-readelf STRIP=llvm-strip HOSTAR=llvm-ar HOSTAS=llvm-as HOSTNM=llvm-nm HOSTLD=ld.lld
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
