e6c2fb45f094bc9e3b6579ca1c9beb4a6dd32a88
rayrzy <vincentrayananda@gmail.com>
2021-12-24 10:15:44 +0700

    vince: rootdir: Drop init.qcom.usb.sh
    * Drop net_raw groups causing useless denials
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- device.mk
+++ device.mk
@@ -362,7 +362,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.recovery.qcom.usb.rc \
     init.target.rc \
