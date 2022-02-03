e54101de21843fdf17f6c185b8792aa447b74654
rayrzy <vincentrayananda@gmail.com>
2022-02-03 12:31:42 +0700

    vince: drop APN config
    We don't need to prebuilt APN from device side, better drop it.
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- device.mk
+++ device.mk
@@ -86,10 +86,6 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
-# APNS Config
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
