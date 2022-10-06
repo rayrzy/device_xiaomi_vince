5fba2382c8da95288eea14a03f3ada05cafbb56c
rayrzy <vincentrayananda@gmail.com>
2022-10-07 01:47:33 +0700

    Revert "vince: Build dummy android.hidl.manager@1.0"
    This reverts commit 114c29df566e499b24e75e73632b063266b618b8.
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- device.mk
+++ device.mk
@@ -229,9 +229,7 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.base@1.0
 
 # HW crypto
 PRODUCT_PACKAGES += \
