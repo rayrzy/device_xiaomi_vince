c85a95978b378719f03f1d77c3e246e58cbd2ceb
rayrzy <vincentrayananda@gmail.com>
2022-10-19 01:27:19 +0700

    vince: fix proton clang path
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: Ic2e86d737d527c0ae3f26b45ef0b1c80859c062f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/vince
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton-clang
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # ANT
