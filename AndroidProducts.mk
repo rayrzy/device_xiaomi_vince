addd3bcfe7d3d8541d868f34578d3aaa556020da
rayrzy <vincentrayananda@gmail.com>
2023-05-06 22:03:34 +0700

    vince: QASSA-ify
    Change-Id: I272d0d30507a36aaadf3cdee1fd8ab4966880857
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vince.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_vince-user \
-    lineage_vince-userdebug \
-    lineage_vince-eng
+    $(LOCAL_DIR)/qassa_vince.mk
